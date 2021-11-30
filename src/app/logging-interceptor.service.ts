ae73c38b21a0e503d9d06121cedba7ec49269a10
profile4vaibhav <mail4vaibhavgadge@gmail.com>
2021-11-30 22:36:32 +0530

    HTTP : Day 2 Finished

---
+++ src/app/logging-interceptor.service.ts
@@ -0,0 +1,8 @@
+import { HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+
+export class LoggingInterceptorService implements HttpInterceptor{
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
+    console.log('Logging Interceptor');
+    return next.handle(req);
+  }
+}
